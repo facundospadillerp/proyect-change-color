0d5d1901e60f17636357f8d2ed8140613d1e5b8e
valeria morales <moralesvaleria6@gmail.com>
2024-05-29 20:49:35 -0300

    cambio de tipografia

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+const changeColorfdc437 = () => {
+    document.getElementById("lorem").style.color = "#fdc437"
+}
+document.getElementById("colorfdc437").addEventListener("clic".changeColorfdc437)
+
+const changeColorfdc437 = () => {
+    document.getElementById("lorem").style.color = "#0c6834"
+}
+document.getElementById("color0c6834").addEventListener("clic".changeColor0c6834)
+
+const changeColorfdc437 = () => {
+    document.getElementById("lorem").style.color = "#4a9ccf"
+}
+document.getElementById("color4a9ccf").addEventListener("clic".changeColor4a9ccf)
+
+const changeColorfdc437 = () => {
+    document.getElementById("lorem").style.color = "#661f45"
+}
+document.getElementById("color661f45").addEventListener("clic".changeColor661f45)
+
+const changeColorfdc437 = () => {
+    document.getElementById("lorem").style.color = "#f43e3d"
+}
+document.getElementById("colorf43e3d").addEventListener("clic".changeColorf43e3d)
